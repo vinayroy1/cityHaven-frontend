c58b975a284f3594a163babbb656c12a27b888af
Vinay Kumar <vinaykumar@Vinays-MacBook-Pro.local>
2025-11-26 08:20:15 +0530

    fixed console error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="light" suppressHydrationWarning style={{ colorScheme: "light" }}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
